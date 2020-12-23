cc9cd936eb8512d7c0964628d19437c4ade965c2
Natthapong <natthapong41@gmail.com>
2020-12-23 10:36:59 +0700

    move content from landingpage html to ts

--- src/app/landing-page/base/base.component.ts
+++ src/app/landing-page/base/base.component.ts
@@ -6,6 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./base.component.css'],
 })
 export class BaseComponent implements OnInit {
+  topics: Array<any> = [
+    {
+      imgUrl: '../../../assets/24-hours.png',
+      contentHeader: '24 hrs. claim',
+      contentDescription: 'Claim Online anywhere & anytime',
+    },
+    {
+      imgUrl: '../../../assets/hospital.png',
+      contentHeader: 'Hospital Link',
+      contentDescription:
+        'Find nearby hospital and Assistance when you have emergency situation',
+    },
+    {
+      imgUrl: '../../../assets/car-repair.png',
+      contentHeader: 'Car repairing',
+      contentDescription: 'Easy to find dealer & Garage',
+    },
+    {
+      imgUrl: '../../../assets/journey2.png',
+      contentHeader: 'Repair tracking',
+      contentDescription: 'Online tracking your car anytime',
+    },
+    {
+      imgUrl: '../../../assets/map.png',
+      contentHeader: 'GPS',
+      contentDescription:
+        'Our implemented feature can bring you to your destination',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
