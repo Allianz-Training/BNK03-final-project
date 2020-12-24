83f4fd817d99974c4aea9bdfb38d0c06499b6f3c
nadiandnd <ndtaeminnie@gmail.com>
2020-12-24 18:33:24 +0700

    sign up base

--- src/app/sign-up/base/base.component.ts
+++ src/app/sign-up/base/base.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RegisterInfo } from '../register-info';
 
 @Component({
-  selector: 'app-base',
+  selector: 'app-signup-base',
   templateUrl: './base.component.html',
   styleUrls: ['./base.component.css']
 })
 export class BaseComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  firstnameip:string='';
+  @Input()
+  lastnameip:string='';
+  @Input()
+  emailip:string='';
+  @Input()
+  insuranceip:string='';
+  @Input()
+  newpassip:string='';
+  @Input()
+  confirmip:string='';
+
+  info: RegisterInfo[];
+  constructor() {
+    this.info = [];   
+    }
 
   ngOnInit(): void {
+  }
+  push(){
+    this.info.push(
+      {
+        firstName: this.firstnameip,
+        lastName: this.lastnameip,
+        email: this.emailip,
+        insuranceNum: this.insuranceip,
+        newPassword: this.newpassip,
+        comfirmPassword: this.confirmip
+      });
+      console.log(this.info)
   }
 
 }
---
+++ src/app/sign-up/register-info.ts
@@ -0,0 +1,8 @@
+export class RegisterInfo {
+    firstName: string;
+    lastName: string;
+    email: string;
+    insuranceNum: string;
+    newPassword: string;
+    comfirmPassword: string;
+}
