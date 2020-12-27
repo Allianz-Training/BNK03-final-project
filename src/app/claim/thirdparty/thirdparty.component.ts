34b37a71c3ac70c2020cd85b88545e314d0c5abb
nadiandnd <ndtaeminnie@gmail.com>
2020-12-27 18:31:19 +0700

    third party

--- src/app/claim/thirdparty/thirdparty.component.ts
+++ src/app/claim/thirdparty/thirdparty.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-thirdparty',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThirdpartyComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private builder:FormBuilder) { 
+    this.form=this.builder.group({
+      firstname:[''],
+      lastName:[''],
+      carRegistNum:[''],
+      phoneNum:[''],
+      date:[''],
+      time:['']
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  push() {
+    console.log(this.form.value)
+  }
 }
